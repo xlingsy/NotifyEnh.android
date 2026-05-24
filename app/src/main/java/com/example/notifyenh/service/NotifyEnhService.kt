cff531b8aef744ce6c4a67fa994ec457cb749da8
kunfei <gekunfei@live.com>
2026-05-24 22:57:05 +0800

    init

--- app/src/main/java/com/example/notifyenh/service/NotifyEnhService.kt
+++ app/src/main/java/com/example/notifyenh/service/NotifyEnhService.kt
@@ -6,8 +6,14 @@ import android.service.notification.StatusBarNotification
 class NotifyEnhService: NotificationListenerService() {
 
 
-    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
+        // 提取通知信息
+        val pkg = sbn.packageName
+        val notification = sbn.notification
+        val extras = notification.extras
+        val title = extras.getString(android.app.Notification.EXTRA_TITLE, "")
+        val text = extras.getString(android.app.Notification.EXTRA_TEXT, "")
     }
 
 }
